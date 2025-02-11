b2e4d5684ddde93f01152a7803f0174fdcbe2639
Do Thi Hang Ha <108347421+dohangha@users.noreply.github.com>
2025-02-11 18:15:20 +0800

    first commit

---
+++ src/sections/index.ts
@@ -0,0 +1,10 @@
+import CustomerReviews from './CustomerReviews';
+import Hero from './Hero';
+import Services from './Services';
+import PopularProducts from './PopularProducts';
+import SpecialOffer from './SpecialOffer';
+import Subcribe from './Subcribe';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+
+export {Hero, CustomerReviews, Services, PopularProducts, SpecialOffer, Subcribe, SuperQuality, Footer}
